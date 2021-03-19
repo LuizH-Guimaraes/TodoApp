077a020de0f67a86518e270175b7713ca0d1b326
PauloMesquita <p.mesquita@vp6.com.br>
2021-03-19 19:30:22 -0300

    body-parser=>express and crud to services folder

--- backend/src/loaders/express.js
+++ backend/src/loaders/express.js
@@ -1,10 +1,10 @@
-const cors = require('cors')
-const {json} = require('body-parser')
-const routes = require('../api')
-const config = require('../config')
+const cors = require("cors");
+const { json } = require("express");
+const routes = require("../api");
+const config = require("../config");
 
 module.exports = (app) => {
-    app.use(cors())
-    app.use(json())
-    app.use(config.api.prefix, routes())
-}
+  app.use(cors());
+  app.use(json());
+  app.use(config.api.prefix, routes());
+};
