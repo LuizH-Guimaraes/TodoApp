811d0fe6c2d4ab3de0936d1d8c1d5747e902e401
PauloMesquita <p.mesquita@vp6.com.br>
2021-03-27 12:44:32 -0300

    authentication

--- backend/src/api/utils/template.js
+++ backend/src/api/utils/template.js
@@ -1,37 +1,45 @@
 const RequestResponse = require("./request_response");
+const JwtVerify = require("../../middleware/jwt");
 
 module.exports = (
   crudService,
   route,
   tableName,
-  methods = ["GET", "POST", "PATCH", "DELETE"]
+  methods = ["GET", "POST", "PATCH", "DELETE"],
+  authenticate = true
 ) => {
   const requestResponse = new RequestResponse(tableName);
 
+  const middlewareFunction = authenticate
+    ? JwtVerify
+    : (req, res, next) => {
+        next();
+      };
+
   if (methods.includes("GET")) {
-    route.get("/", async (req, res) => {
+    route.get("/", middlewareFunction, async (req, res) => {
       return requestResponse.sendDbResult(res, crudService.list());
     });
 
-    route.get("/count", async (req, res) => {
+    route.get("/count", middlewareFunction, async (req, res) => {
       return requestResponse.sendDbResult(res, crudService.count());
     });
 
-    route.get("/:id", (req, res) => {
+    route.get("/:id", middlewareFunction, (req, res) => {
       const id = requestResponse.getParam(req, res, "id");
       return requestResponse.sendDbResult(res, crudService.get(id));
     });
   }
 
   if (methods.includes("POST")) {
-    route.post("/", (req, res) => {
+    route.post("/", middlewareFunction, (req, res) => {
       const data = requestResponse.getData(req, res);
       return requestResponse.sendDbResult(res, crudService.create(data));
     });
   }
 
   if (methods.includes("PATCH")) {
-    route.patch("/:id", (req, res) => {
+    route.patch("/:id", middlewareFunction, (req, res) => {
       const id = requestResponse.getParam("id");
       const data = requestResponse.getData(req, res);
       return requestResponse.sendDbResult(res, crudService.edit(data, id));
@@ -39,7 +47,7 @@ module.exports = (
   }
 
   if (methods.includes("DELETE")) {
-    route.delete("/:id", (req, res) => {
+    route.delete("/:id", middlewareFunction, (req, res) => {
       const id = requestResponse.getParam(req, res, "id");
       return requestResponse.sendDbResult(res, crudService.delete(id));
     });
---
+++ backend/src/middleware/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { session } = require("../config");
+
+// Verify authentication
+module.exports = (req, res, next) => {
+  const authorizationField = req.headers["authorization"];
+
+  if (!authorizationField)
+    return res.status(401).json({
+      error: true,
+      message: "No token provided.",
+      result: "You need to send a token",
+    });
+  const token = authorizationField.split("Bearer ")[1];
+  jwt.verify(token, session.secret, (err, decoded) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ auth: false, message: "Failed to authenticate token." });
+
+    req.userId = decoded.id;
+    next();
+  });
+};
